7e656f64d79d9d6bfa0556ac601cfe0c6b6d7d95
Mateo Adrian Jijon Cañas <mateo.jijon.canas@udla.edu.ec>
2023-10-25 07:20:30 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MateoJijon_Examen1P.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
