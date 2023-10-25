49cdadb196db460b857b59b49a178c9c74e0167c
Mateo Adrian Jijon Cañas <mateo.jijon.canas@udla.edu.ec>
2023-10-25 07:53:58 -0500

    Creacion de la clase Paciente con 5 Data annotacions, int, string, boll, decimal y fecha

---
+++ Models/Paciente.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MateoJijon_Examen1P.Models
+{
+    public class Paciente
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public String? Nombre { get; set;}
+        [Required]
+        public String? Apellido { get; set;}
+        [Range(1, 100)]
+        public decimal Precio { get; set;}
+        public bool Especialidad { get; set;}
+
+        [DataType(DataType.Date)]
+        public DateTime? Fecha { get; set;}
+
+        [StringLength(100)]
+        public String? Descripcion { get; set;}
+        
+        
+        [RegularExpression(@"^[A-Z]{3}-\d{3}$")]
+        public String? CodigoUnico { get; set;}
+       
+
+    }
+    
+}
